e37e11899e374d5a2247b4199f61bffb135278c4
Francesc Busquets <francesc@gmail.com>
2014-01-23 14:59:22 +0100

    Modified build.xml to allow signing jar files

--- qv_applets/data/cert.bat
+++
@@ -1,7 +0,0 @@
-keytool -genkey -keyalg RSA -dname "cn=Quaderns Virtuals, ou=Projectes TIC,o=Departament Educacio, L=Barcelona, S=Spain, C=es" -alias qv -keypass virtuals -validity 7305 -keystore qv.keystore -storepass virtuals
-
-keytool -certreq -alias qv -keystore qv.keystore -keypass virtuals -file qv.csr
-
-keytool -export -alias qv -keystore qv.keystore -rfc -file qv.cer
-
-keytool -import -alias qvcert -file qv.cer -keystore truststore
