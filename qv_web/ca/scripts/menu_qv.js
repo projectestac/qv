1dc4770f42ee4dbc95e6724da7ddaccdacdbd45d
pau.ferrer-ocana <crazyserver@gmail.com>
2015-09-10 13:35:34 +0200

    Delete forum broken link and adapt links to MV

--- qv_web/ca/scripts/menu_qv.js
+++ qv_web/ca/scripts/menu_qv.js
@@ -1,6 +1,6 @@
 var menu_n1= new Array(
 	new Array("m_qv","Quaderns Virtuals","qv.htm"),
-	new Array("m_biblio","Biblioteca","http://clic.xtec.cat/qv_biblio/act_list.jsp?lang=ca"),
+	new Array("m_biblio","Biblioteca","/qv_biblio/act_list.jsp?lang=ca"),
 	new Array("m_docs","Documents","docs.htm"),
 	new Array("m_suport","Suport","suport.htm"),
 	new Array("m_comu","Comunitat","comu.htm"),
@@ -12,7 +12,7 @@ var m_qv_n2= new Array(
 	new Array("m_qv_faq","Preguntes freqüents","suport_faq.htm"));
 
 var m_biblio_n2= new Array(
-	new Array("m_biblio_cerca","Cerca","http://clic.xtec.cat/qv_biblio/act_list.jsp?lang=ca"),
+	new Array("m_biblio_cerca","Cerca","/qv_biblio/act_list.jsp?lang=ca"),
 	new Array("m_biblio_aporta","Com aportar quaderns?","biblio_aporta.htm"));
 
 var m_docs_n2= new Array(
@@ -22,7 +22,7 @@ var m_docs_n2= new Array(
 
 var m_suport_n2= new Array(
 	new Array("m_suport_faq","Preguntes freqüents","suport_faq.htm"),
-	new Array("m_suport_forum","Fòrum","http://phobos.xtec.net/qv/forum/index.php"),
+	//new Array("m_suport_forum","Fòrum","http://phobos.xtec.net/qv/forum/index.php"),
 	new Array("m_suport_bustia","Bústia","suport_bustia.htm"));
 
 var m_comu_n2= new Array(
@@ -30,6 +30,6 @@ var m_comu_n2= new Array(
 	new Array("m_comu_links","Enllaços","comu_links.htm"));
 
 var m_idioma_n2= new Array(
-	new Array("m_idioma_ca","Català","http://clic.xtec.cat/qv_web/ca/index.htm"),
-	new Array("m_idioma_es","Español","http://clic.xtec.cat/qv_web/es/index.htm"),
-	new Array("m_idioma_en","English","http://clic.xtec.cat/qv_web/en/index.htm"));
+	new Array("m_idioma_ca","Català","/qv_web/ca/index.htm"),
+	new Array("m_idioma_es","Español","/qv_web/es/index.htm"),
+	new Array("m_idioma_en","English","/qv_web/en/index.htm"));
